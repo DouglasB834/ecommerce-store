00c479819a7965864f3388313885a4f8361d1adc
DouglasB <douglas_borges09@hmail.com>
2023-10-23 13:32:35 -0300

    feat: add category page

---
+++ src/app/catalogo/page.tsx
@@ -0,0 +1,25 @@
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import React from "react";
+import { CategoryItem } from "./components/category-item";
+
+export default async function page() {
+  const categories = await prismaClient.category.findMany();
+
+  return (
+    <div className="flex flex-col gap-8">
+      <Badge
+        className="w-fit uppercase px-[12px] py-[5px] border-primary font-bold text-base gap-1 "
+        variant={"outline"}
+      >
+        <ShapesIcon size={16} /> Catálogo
+      </Badge>
+      <ul className="grid grid-cols-2 gap-8">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </ul>
+    </div>
+  );
+}
