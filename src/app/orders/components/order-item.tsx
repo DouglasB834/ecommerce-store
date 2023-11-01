f117bd51427b7f211ad4a49b3839d7fd1fe9855d
DouglasB <douglas_borges09@hmail.com>
2023-11-01 13:53:43 -0300

    feat: add page my order and show orders

---
+++ src/app/orders/components/order-item.tsx
@@ -0,0 +1,58 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Card } from "@/components/ui/card";
+import { Prisma } from "@prisma/client";
+import { format } from "date-fns";
+
+interface IOrderItemProps {
+  order: Prisma.OrderGetPayload<{
+    include: {
+      orderProducts: true;
+    };
+  }>;
+}
+//collapsible
+export const OrderItem = ({ order }: IOrderItemProps) => {
+  return (
+    <Card className="px-4">
+      <Accordion type="single" collapsible className="">
+        <AccordionItem value={`${order.id}`}>
+          <AccordionTrigger>
+            <div className="flex flex-col items-start gap-2">
+              <h3 className="font-semibold text-sm">Número do pedido</h3>
+              <p className="text-xs">Pedido: 002 </p>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            {/* informaçãp do produto  */}
+            <div className="flex flex-col ">
+              <div className="flex justify-between  items-center">
+                <div>
+                  {/* status */}
+                  <p className="uppercase font-semibold">Status</p>
+                  <span className="text-[#8162FF]">{order?.status}</span>
+                </div>
+                <div>
+                  {/* data */}
+                  <p className="uppercase font-semibold">Data</p>
+                  <span className="opacity-75">
+                    {format(order.createAt, "dd-mm-yyyy")}
+                  </span>
+                </div>
+                <div>
+                  {/* forma de pagamento */}
+                  <p className="uppercase font-semibold">Pago</p>
+                  <span className="opacity-75">Cartão</span>
+                </div>
+              </div>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </Card>
+  );
+};
---
+++ src/app/orders/page.tsx
@@ -0,0 +1,40 @@
+import { Badge } from "@/components/ui/badge";
+import { authOptions } from "@/lib/auth";
+import { prismaClient } from "@/lib/prisma";
+import { CreditCardIcon } from "lucide-react";
+import { getServerSession } from "next-auth";
+import { OrderItem } from "./components/order-item";
+
+const OrderPage = async () => {
+  const user = await getServerSession(authOptions); //tras user auth serve side
+
+  if (!user?.user) {
+    return <div> Você não tem permissão para acessar essa página </div>;
+  }
+  const orders = await prismaClient.order.findMany({
+    where: {
+      id: (user as any).id,
+    },
+    include: {
+      orderProducts: true,
+    },
+  });
+
+  return (
+    <div className="flex flex-col p-5 gap-4">
+      <Badge
+        className="w-fit uppercase px-[12px] py-[5px] border-primary font-bold text-base gap-1 "
+        variant={"outline"}
+      >
+        <CreditCardIcon size={16} /> Meus Pedidos
+      </Badge>
+      <ul className="flex flex-col gap-5">
+        {orders.map((order) => (
+          <OrderItem key={order.id} order={order} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default OrderPage;
