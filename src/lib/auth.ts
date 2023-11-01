012bd342b6f0690a4a03dacc135927ce722fa076
DouglasB <douglas_borges09@hmail.com>
2023-11-01 11:46:58 -0300

    fix:  remove if if set localstorege  and add callbacks authNext to get id user

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,4 +12,16 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    //para pegar o id callback nao vem por padrão
+    async session({ session, token, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
+
+      return session;
+    },
+  },
 };
